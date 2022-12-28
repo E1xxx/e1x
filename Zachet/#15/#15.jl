de36d6be6677c7e4138356c047ea011677430dec
E1xxx <121410482+E1xxx@users.noreply.github.com>
2022-12-28 09:43:32 +0300

    Update #15.jl

--- Zachet/#15/#15.jl
+++ Zachet/#15/#15.jl
@@ -1,30 +1,15 @@
 using HorizonSideRobots
 
-function n15!(robot)
-    putmarker!(robot)
-    for side in (Nord,West,Sud,Ost)
-        n=mark_crest!(robot,side)
-        along!(robot,inverse(side),n)
-    end
-end
+function find_space!(r::Robot, side_to_wall::HorizonSide)
+    n_steps = 1
+    side = next_side(side_to_wall)
 
-function mark_crest!(robot,side)
-    num_steps=0
-    while !isborder(robot,side) && !isborder(robot,next(side))
-        move!(robot,side)
-        move!(robot,next(side))
-        putmarker!(robot)
-        num_steps+=1
+    while isborder(r, side_to_wall)
+        for _ in 1:n_steps
+            shatl!( _ -> !isborder(r, side_to_wall), r, side)
+        end
+        side = inverse_side(side)
+        n_steps += 1
     end
-    return num_steps
-end
 
-function along!(robot,side,num_steps)
-    for _ in 1:num_steps
-        move!(robot,side)
-        move!(robot,next(side))
-    end
 end
-
-inverse(side::HorizonSide)=HorizonSide((Int(side)+2)%4)
-next(side::HorizonSide)=HorizonSide((Int(side)+1)%4)
