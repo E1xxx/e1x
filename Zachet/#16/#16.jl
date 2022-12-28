eb917a66b1acc54ffe0a302d5b537be6b97cac81
E1xxx <121410482+E1xxx@users.noreply.github.com>
2022-12-28 09:48:50 +0300

    Update #16.jl

--- Zachet/#16/#16.jl
+++ Zachet/#16/#16.jl
@@ -2,3 +2,16 @@ function find_marker!(r::Robot)
     tmp = (side::HorizonSide) -> ismarker(r)
     spiral!( tmp, r)
 end
+
+function spiral!(robot, stop_condition::Function)
+    side=Nord
+    n=1
+    along!(robot, side,n)
+    while !stop_condition(robot)
+        for _ in 1:2
+            along!(robot, side, n)
+            side = next(side)
+        end
+        n += 1
+    end
+end
